c4f67da811c736021ba718d65df61846c29cc53c
yo123abxd <yo123abxd@gmail.com>
2024-07-16 08:52:50 +0000

    init

--- edgelist2mtx.py
+++ edgelist2mtx.py
@@ -8,8 +8,6 @@ if __name__ == "__main__":
     for f_path in files:
         f_name = f_path.split(".txt")[0]
         directed = False if "undirected" in f_path else True
-        if f_name+".txt" in files_k_core:
-            directed = False
 
         g = nx.read_edgelist(f_path, create_using=nx.Graph if "undirected" in f_path else nx.DiGraph)
         m = nx.to_scipy_sparse_array(g)
